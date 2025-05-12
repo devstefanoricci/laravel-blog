224800946b12c36982ecd3d66ee82b9fb0e7c945
devstefanoricci <stefanoricci.eu@gmail.com>
2025-05-12 12:06:44 +0200

    [VIEW] added menù, error on trashed (TBD)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,17 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="{{route('posts.index')}}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="{{route('posts.create')}}">New note</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('posts.trashed')}}">Trashed</a>
+                          </li>
+                    </ul>
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
 
